8c21533ed6c33d418826644a52beff63f9185887
SeyoungOh <33598679+driffe@users.noreply.github.com>
2023-12-26 14:04:50 +0000

    change some style and fix error on hint part

--- mathQuiz.js
+++ mathQuiz.js
@@ -16,9 +16,9 @@ function openQuiz(evt, quiz) {
 
 // Function to check Quiz 01 answer
 function check01() {
-    const finalAns = 4;
-    const element01 = document.getElementById("ansBox01");
-    const tempAns = element01 ? element01.value : 0;
+    var finalAns = 4;
+    var element01 = document.getElementById("ansBox01");
+    var tempAns = element01 ? element01.value : 0;
 
     if (parseInt(tempAns) === finalAns) {
         // Block the input and provide feedback
@@ -31,10 +31,10 @@ function check01() {
 
 // Function to provide a hint for Quiz 01
 function hintCheck01() {
-    const value01 = parseInt(document.getElementById("textbox01-1").value) || 0;
-    const value02 = parseInt(document.getElementById("textbox02-1").value) || 0;
+    var value01 = parseInt(document.getElementById("textbox01-1").value) || 0;
+    var value02 = parseInt(document.getElementById("textbox02-1").value) || 0;
 
-    let ans;
+    var ans;
     if (value01 < value02) {
         ans = value02 % value01;
     } else {
@@ -46,9 +46,9 @@ function hintCheck01() {
 
 // Function to check Quiz 02 answer
 function check02() {
-    const finalAns = 9;
-    const element02 = document.getElementById("ansBox02");
-    const tempAns = element02 ? element02.value : 0;
+    var finalAns = 9;
+    var element02 = document.getElementById("ansBox02");
+    var tempAns = element02 ? element02.value : 0;
 
     if (parseInt(tempAns) === finalAns) {
         // Block the input and provide feedback
@@ -60,26 +60,27 @@ function check02() {
 
 // Function to provide a hint for Quiz 02
 function hintCheck02() {
-    const value01 = parseInt(document.getElementById("textbox01-2").value) || 0;
-    const value02 = parseInt(document.getElementById("textbox02-2").value) || 0;
+    var value01 = parseInt(document.getElementById("textbox01-2").value) || 0;
+    var value02 = parseInt(document.getElementById("textbox02-2").value) || 0;
 
-    const temp = value01 + value02;
+    var temp = value01 + value02;
     while(true) {
-        temp = temp % 12;
         if(temp > 12) {
             break;
+        } else {
+            temp = temp % 12;
         }
     }
-    let ans = temp;
+    var ans = temp;
 
     document.getElementById("result02").value = ans;
 }
 
 // Function to check Quiz 03 answer
 function check03() {
-    const finalAns = 277;
-    const element03 = document.getElementById("ansBox03");
-    const tempAns = element03 ? element03.value : 0;
+    var finalAns = 277;
+    var element03 = document.getElementById("ansBox03");
+    var tempAns = element03 ? element03.value : 0;
 
     if (parseInt(tempAns) === finalAns) {
         // Block the input and provide feedback
@@ -91,21 +92,21 @@ function check03() {
 
 // Function to provide a hint for Quiz 03 (17 ? 10 = 277) 
 function hintCheck03() {
-    const value01 = parseInt(document.getElementById("textbox01-3").value) || 0;
-    const value02 = parseInt(document.getElementById("textbox02-3").value) || 0;
+    var value01 = parseInt(document.getElementById("textbox01-3").value) || 0;
+    var value02 = parseInt(document.getElementById("textbox02-3").value) || 0;
 
-    const result01 = value01 + value02;
-    const result02 = Math.abs(value01 - value02);
-    const ans = result01.toString() + result02.toString();
+    var result01 = value01 + value02;
+    var result02 = Math.abs(value01 - value02);
+    var ans = result01.toString() + result02.toString();
 
     document.getElementById("result03").value = ans;
 }
 
 // Function to check Quiz 04 answer
 function check04() {
-    const finalAns = 37;
-    const element04 = document.getElementById("ansBox04");
-    const tempAns = element04 ? element04.value : 0;
+    var finalAns = 37;
+    var element04 = document.getElementById("ansBox04");
+    var tempAns = element04 ? element04.value : 0;
 
     if (parseInt(tempAns) === finalAns) {
         element04.disabled = true;
@@ -116,9 +117,9 @@ function check04() {
 
 // Function to provide a hint for Quiz 04
 function hintCheck04() {
-    const value01 = parseInt(document.getElementById("textbox01-4").value) || 0;
-    const value02 = parseInt(document.getElementById("textbox02-4").value) || 0;
-    const ans = 0;
+    var value01 = parseInt(document.getElementById("textbox01-4").value) || 0;
+    var value02 = parseInt(document.getElementById("textbox02-4").value) || 0;
+    var ans = 0;
 
     if(value01 > value02) {
         ans = value01 * 2 - value02;
